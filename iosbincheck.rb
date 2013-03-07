df1ac6f411f4e2bd9f2e9b143275c24864e1a503
michael <mgianarakis@mgsecurus.local>
2013-03-08 10:34:25 +1100

    Added symbol table dump and class dump as well as quick bug fix

--- iosbincheck.rb
+++ iosbincheck.rb
@@ -10,11 +10,11 @@ end
 
 binary = File.basename( path_to_binary, ".*" )
 
-output = File.new( binary + "_binary_check)output.txt", "w+" )
+output = File.new( binary + "_binary_checks.txt", "w+" )
 
 #PIE Check
 
-pie_check = `otool -hv #{ binary } | grep PIE`
+pie_check = `otool -hv #{ path_to_binary } | grep PIE`
 
 output.puts "POSITION INDEPENDENT EXECUTABLE:\n\n"
 
@@ -24,12 +24,12 @@ else
 	output.puts "Binary has been compiled with PIE\n\n"
 end
 
-output.puts "$ otool -hv #{ binary } | grep PIE\n"
+output.puts "$ otool -hv #{ path_to_binary } | grep PIE\n"
 output.puts "#{ pie_check.strip() }\n\n"
 
 #Stack Smashing Protection Check
 
-stack_check = `otool -Iv #{ binary } | grep stack`
+stack_check = `otool -Iv #{ path_to_binary } | grep stack`
 
 output.puts "STACK SMASHING PROTECTION:\n\n"
 
@@ -39,12 +39,12 @@ else
 	output.puts "Binary has been compiled with Stack Smashing Protection\n\n"
 end
 
-output.puts "$ otool -Iv #{ binary } | grep stack\n"
+output.puts "$ otool -Iv #{ path_to_binary } | grep stack\n"
 output.puts "#{ stack_check.strip() }\n\n"
 
 #ARC Check
 
-arc_check = `otool -Iv #{ binary } | grep _objc_release`
+arc_check = `otool -Iv #{ path_to_binary } | grep _objc_release`
 
 output.puts "AUTOMATIC REFERENCE COUNTING:\n\n"
 
@@ -54,8 +54,23 @@ else
 	output.puts "Application uses Automatic Reference Counting\n\n"
 end
 
-output.puts "$ otool -Iv #{ binary } | grep _objc_release\n"
+output.puts "$ otool -Iv #{ path_to_binary } | grep _objc_release\n"
 output.puts "#{ arc_check.strip() }\n\n"
 
 #Symbol Table
+
+symbol_table = File.new( binary + "_symbol_table.txt", "w+" )
+symbol_table.puts "#{ `nm #{ path_to_binary }` }"
+
+#Class Dump
+
+class_dump = File.new( binary + "_class_dump.txt", "w+" )
+
+class_dump_check = `class-dump-z -A #{ path_to_binary }`
+
+if class_dump_check =~ /Command Not Found/
+	class_dump.puts "class-dump-z is not installed on this system or not in your PATH."
+else
+	class_dump.puts class_dump_check
+end
 
